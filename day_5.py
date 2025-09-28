1ba456324b6d991fbaa10b732748727d6dd0c9fb
Mandip Sapkota <mandeepsapkota13@gmail.com>
2025-09-28 22:59:18 +0545

    day 5 done

---
+++ day_5.py
@@ -0,0 +1,26 @@
+# Top k frequent
+from typing import List
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        freq = [[] for i in range (len(nums)+1)]
+
+        for n in nums:
+            count[n] = 1+count.get(n,0)
+
+        for n,c in count.items():
+            freq[c].append(n)
+        
+        res = []
+        for i in range(len(freq)-1,0,-1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
+                
+if __name__ == "__main__":
+    solution = Solution()
+    list1 = [1,2,4,2,2,3,4]
+    k = 2
+
+    print(solution.topKFrequent(list1, k))

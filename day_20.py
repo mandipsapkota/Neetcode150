22b665914c584370187331a8ae4f3781e3a0eb65
Mandip Sapkota <mandeepsapkota13@gmail.com>
2025-10-13 21:53:24 +0545

    Day 20 - Sliding Window Maximum - Brute Force - Solution Added

---
+++ day_20.py
@@ -0,0 +1,12 @@
+# BRUTE FORCE 
+class BruteForceSolution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        l,r = 0,k
+        maxList = []
+        while r<=len(nums):
+            element = sorted(nums[l:r])[-1]
+            maxList.append(element)
+            l+=1
+            r+=1
+        
+        return maxList

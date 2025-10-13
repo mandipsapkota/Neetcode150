770f8a04a67894039afecf39879c9f5039d9c788
Mandip Sapkota <mandeepsapkota13@gmail.com>
2025-10-13 22:22:54 +0545

    Day 20 - Sliding Window Maximum - Deque - Solution Added

--- day_20.py
+++ day_20.py
@@ -1,5 +1,7 @@
-# BRUTE FORCE 
 from typing import List
+import collections
+
+# BRUTE FORCE 
 class BruteForceSolution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         l,r = 0,k
@@ -11,3 +13,35 @@ class BruteForceSolution:
             r+=1
         
         return maxList
+    
+# DEQUE
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        res = []
+        q = collections.deque()
+        l = r = 0
+        while r<len(nums):
+            # pop smaller values from q
+            while q and nums[q[-1]] < nums[r]:
+                q.pop()
+            
+            q.append(r)
+
+            # remove left val from window 
+            if l>q[0]:
+                q.popleft()
+
+            if (r+1)>=k:
+                res.append(nums[q[0]])
+                l+= 1
+
+            r += 1
+        return res
+    
+# INFERENCE
+
+if __name__ == "__main__":
+    solution = Solution()
+    nums= [2,5,3,5,3,6,3,5,3,6,4]
+    k = 3
+    print(solution.checkInclusion(nums,k))

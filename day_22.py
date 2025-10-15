6def1ddd03e56dcb3400a0a5efb401b6c66ba7a6
Mandip Sapkota <mandeepsapkota13@gmail.com>
2025-10-15 22:01:44 +0545

    Day 22 - Minimum Stack - Solution Added

---
+++ day_22.py
@@ -0,0 +1,31 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        val = min(val, self.minStack[-1] if self.minStack else val)
+        self.minStack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
+
+
+if __name__ == "__main__":
+    minStack = MinStack()
+    minStack.push(-2)
+    minStack.push(0)
+    minStack.push(-3)
+    print(minStack.getMin())  # Output: -3
+    minStack.pop()
+    print(minStack.top())     # Output: 0
+    print(minStack.getMin())  # Output: -2

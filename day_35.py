c539e44e4ce79f7535428a70e8c9608519a63232
Mandip Sapkota <mandeepsapkota13@gmail.com>
2025-10-28 20:46:05 +0545

    Day 35 - Reverse Linked List -Recursive Solution Added

--- day_35.py
+++ day_35.py
@@ -1,14 +1,17 @@
+from typing import Optional
+
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 
 # Iterative Solution 
 
 class IterativeSolution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        prev,curr = None,head
+        prev, curr = None, head
 
         while curr:
             nxt = curr.next
@@ -17,3 +20,36 @@ class IterativeSolution:
             curr = nxt
         
         return prev
+    
+
+class RecursiveSolution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return None
+        
+        newHead = head
+        if head.next:
+            newHead = self.reverseList(head.next)
+            head.next.next = head
+
+        head.next = None
+        return newHead
+    
+if __name__ == "__main__":
+    solution = IterativeSolution()
+    solution2 = RecursiveSolution()
+
+    # Example usage:
+    # Creating a linked list: 1 -> 2 -> 3 -> 4 -> 5
+    head = ListNode(1)
+    head.next = ListNode(2)         
+    head.next.next = ListNode(3)
+    head.next.next.next = ListNode(4)
+    head.next.next.next.next = ListNode(5)
+    #  Testing Iterative Solution
+    reversed_head_iterative = solution.reverseList(head)
+    #  Testing Recursive Solution
+    reversed_head_recursive = solution2.reverseList(head)
+
+    print(reversed_head_iterative)
+    print(reversed_head_recursive)

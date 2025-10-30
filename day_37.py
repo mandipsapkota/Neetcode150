5d69f7df1160f998b9ecd62d881bc07768b99e6f
Mandip Sapkota <mandeepsapkota13@gmail.com>
2025-10-30 22:22:58 +0545

    Day 37 - Linked List Cycle Detection - Tortroise Hare Solution Added

---
+++ day_37.py
@@ -0,0 +1,29 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow, fast = head, head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if fast == slow:
+                return True
+        return False
+
+if __name__ == "__main__":
+    # Create a linked list with a cycle
+    node1 = ListNode(3)
+    node2 = ListNode(2)
+    node3 = ListNode(0)
+    node4 = ListNode(-4)
+    
+    node1.next = node2
+    node2.next = node3
+    node3.next = node4
+    node4.next = node2  # Creates a cycle
+
+    solution = Solution()
+    print(solution.hasCycle(node1))  # Output: True

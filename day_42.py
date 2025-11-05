79b03d91a28dbf6c3a4c01e469e2897796167de2
Mandip Sapkota <mandeepsapkota13@gmail.com>
2025-11-05 22:14:39 +0545

    Day 42 - Find the duplicate number - Hashset solution added

---
+++ day_42.py
@@ -0,0 +1,9 @@
+from typing import List
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return num
+            seen.add(num)
+        return -1
